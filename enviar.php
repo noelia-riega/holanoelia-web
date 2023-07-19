dc48b0367b9424a79149ac8bcafcb6ffcb197488
Noelia-Ainsworth <noeliariega@gmail.com>
2023-07-19 10:51:29 -0400

    php formulario y html, css arigatito mensaje

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+$destino = "contacto@holanoelia.com";
+$nombre  = $_POST["nombre"];
+$email   = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . ".\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+mail($destino,"Hablemos", $contenido)
+header("Location:arigatitos.html");
+
+?>
