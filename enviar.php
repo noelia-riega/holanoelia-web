a614fd3742e30f99fa1e6f766cd3d735761ee129
Noelia-Ainsworth <noeliariega@gmail.com>
2023-07-21 22:13:08 -0400

    ok

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino = "contacto@holanoelia.com";
-$nombre  = $_POST["nombre"];
-$email   = $_POST["email"];
-$mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre . ".\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
-mail($destino,"Hablemos", $contenido)
-header("Location:arigatitos.html");
-?>
